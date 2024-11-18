dfa33d712c03944fc4d076f9824540d83b07a12d
Moh1t0 <[vanya.kostenko05@mail.ru]>
2024-11-18 17:12:37 +0500

    Initial commit

---
+++ src/main/java/com/javaacademy/nuclearstation/NuclearstationApplication.java
@@ -0,0 +1,13 @@
+package com.javaacademy.nuclearstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuclearstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuclearstationApplication.class, args);
+	}
+
+}
